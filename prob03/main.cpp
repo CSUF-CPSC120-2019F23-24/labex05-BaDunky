3aa0862a2bb729f81ca41ddd5d8ae4dad78a071c
BaDunky <arturitosilva20@hotmail.com>
2019-09-21 14:13:05 -0700

    Random Rectangle Generator

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,23 @@
 // Random Rectangle
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+int main(){
+  unsigned seed = time(0);
+  srand(seed);
+
+  std::string box_string;
+  int max_length = 40;
+  int length = rand() % max_length + 1;
+
+  box_string.assign(length, '#');
+
+  std::cout << box_string << std::endl;
+  std::cout << box_string << std::endl;
+  std::cout << box_string << std::endl;
+  
+  return 0;
+}
