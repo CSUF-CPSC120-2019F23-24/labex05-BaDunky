e6400ecd0bd48cf7750a2e823c56c9a73e49ca40
Arturo Silva <arturitosilva20@hotmail.com>
2019-10-09 16:36:17 -0700

    changed MAX_LENGTH into constant variable

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -10,14 +10,14 @@ int main(){
   srand(seed);
 
   std::string box_string;
-  int max_length = 40;
-  int length = rand() % max_length + 1;
+  const int MAX_LENGTH = 40;
+  int length = rand() % MAX_LENGTH + 1;
 
   box_string.assign(length, '#');
 
   std::cout << box_string << std::endl;
   std::cout << box_string << std::endl;
   std::cout << box_string << std::endl;
-  
+
   return 0;
 }
